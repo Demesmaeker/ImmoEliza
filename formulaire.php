d499aa0d12384fc0a3248f148ee1dfbf70c7f7f9
Kultigin Karagenc <karagenc.kultigin@gmail.com>
2020-11-27 14:10:40 +0100

    formulaire start

--- formulaire.php
+++ formulaire.php
@@ -1,41 +1,36 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Formulaire IMMO</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    
-    <body>
-        <h1>Connaitre la valeur de votre bien immobilier</h1>
-        <?php
-            $maison = ['Batiment de service' => 1, 'Bungaloo' => 2 , 'Duplex' => 3, 'Maison' => 4, 'Maison de campagne' => 5, 'Maison de ville' => 6, 'Manoir' => 7, 'Mix' => 8, 'Propriété exceptionnelle' => 9, 'Triplex' => 10, 'Villa' => 11 ];
+<?php
 
-            $appartement = [ 'Appartement' => 12 , 'Kot' => 13 ,'Loft' => 14, 'Penthouse' => 15, 'Rez-de-Chaussée' => 16, 'Studio plein pied' => 17];
+    namespace ImmoEliza;
 
-            $option = ['Surface habitable' => 18, 'Surface terrain (habitation comprise)' => 19, 'Etat du batiment' => 20, 'Neuf' => 20.1, 'Bon' => 20.2, 'A rénover' => 20.2, 'A rafraishir' => 20.3, 'A restaurer' => 20.4, 'Vient d"être rénové' => 20.5, 'Nombre de chambres' => 21, 'Terrasse' => 22, 'Jardin' => 23, 'Nombres de facades' => 24 ];
+        if(isset($_POST['ZIP'], $_POST['type_of_property'],$_POST['subtype_of_property'], $_POST['house_area'], $_POST['surface_of_the_land'])){
+        if(!empty($_POST['ZIP']) && !empty($_POST['type_of_property']) && !empty($_POST['subtype_of_property']) && !empty($_POST['house_area']) && !empty($_POST['surface_of_the_land'])){
+                $ZIP = $_POST['ZIP'];
+                $type_of_property = $_POST['type_of_property'];
+                $subtype_of_property = $_POST['subtype_of_property'];
+                $house_area = $_POST['house_area'];
+                $surface_of_the_land = $_POST['surface_of_the_land'];
+
+                // ---> SANITIZATION <---
+                forEach($_POST as $key => $value){
+                    $value = filter_var($value, FILTER_SANITIZE_STRING);
+                    $value = filter_var($value, FILTER_SANITIZE_ADD_SLASHES);
+                }
+
+                try{
+                    $address = new Address($house_area,$ZIP);
+                    $property = new Property($type_of_property,$subtype_of_property,$surface_of_the_land);
+                    $req = new Request($address,$property);
+                }catch(Exception $err){
+                    echo 'Erreur: '.$err;
+                }
+                
+
+                $tmp = $req->getPrediction();
+                echo '<p class="col-12 text-center" id="priceEstimate" style="background-color: red; font-size: 24px;">Estimation du prix '.$tmp['price_of_the_estate'].'€</p>';
+
+                if(true /* temporaire */){echo '<script type="module" src="./assets/js/objLoad.js"></script>';}
+            }
+        }
 
 
-/*afficher les cat ? 
-if {($maison == $appartement)
-    $appartement = hover;
-} else if {($appartement == $maison)
-    $maison = hover;
-}
-
-*/
-$choix = [$ChoixA, $ChoixM, $ChoixO];
-
-$maison [''] = '$ChoixM';
-$appapartement [''] = '$ChoixA';
-$option [''] = '$ChoixO';
-
-
-            
-            /*foreach($valeur as $Choix => $option){*/
-                echo  'Vous avez selectionez une  ...avec ... est-ce correcte ? ';
-            /*}*/
 ?>
-        <p>Voici une estimation du prix pour votre bien</p>
-    </body>
-</html>
